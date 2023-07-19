1cd0adfad4f3b22cbe133d718b440459ee1e031d
arshiapiri <arshia.piri8585@gmail.com>
2023-07-19 09:50:35 +0330

    Implement user sign-up functionality

---
+++ backEnd/utils/userReq.js
@@ -0,0 +1,10 @@
+module.exports = {
+    userRequestForSignUp: {
+        fristName: String,
+        lastName: String,
+        gender: ["women", "man", "not set"],
+        username: String,
+        password: String,
+        phoneNumber: String
+    }
+};
