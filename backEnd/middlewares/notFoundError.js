1f81c7da6b32a344fdab4debadad44ee058d5317
Reza farhang <rezafarhangian79@gmail.com>
2023-07-23 16:52:54 +0330

    Remove the backend folder

--- backEnd/middlewares/notFoundError.js
+++
@@ -1,6 +0,0 @@
-const AppError = require("../utils/app-error")
-const notFoundError = (req, res, next) => {
-	next(new AppError(404, `${req.method} ${req.originalUrl} not found!`));
-}
-
-module.exports = notFoundError 
--- backEnd/routes/api-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersRouter = require("./auth-router")
-
-
-router.use("/users", usersRouter);
-
-
-module.exports = router;
